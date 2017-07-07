ce626a71847fb3fb7e1de7e66653cdc2222fff1b
Jeff <juratr@gmail.com>
2017-07-06 22:14:47 -0600

    Base start: 	- .gitignore started 	- Canvas black 	- Game scripts empty 	- Toggles selection class in gamesList 	- Pixi.js changes background to white

--- jeffreim/js/jreim.js
+++ jeffreim/js/jreim.js
@@ -13,15 +13,54 @@ var loader;
 var resources;
 var Sprite;
 
+$(document).ready(function(){
+	$("#gamesList li").click(function(event){
+		var target = $(event.target);
+		if(!(target.is($(".selection")))){
+			canvas = document.getElementById("gameCanvas");
+			$(".selection").toggleClass("selection");
+			target.toggleClass("selection");
+			console.log("Switched to \"" + target.text() + "\".");
+		}
+	});
+});
+
 function setup(){
-  canvas = document.getElementById("gameCanvas");
-  rendererOptions = { view: canvas, antialias: false, transparent: false, resolution:1 };
-  autoDetectRenderer = PIXI.autoDetectRenderer;
-  renderer = autoDetectRenderer(canvas.width, canvas.height, rendererOptions);
-  stage = new PIXI.Container();
-  display();
+	canvas = document.getElementById("gameCanvas");
+	rendererOptions = { view: canvas, antialias: false, transparent: false, resolution:1 };
+	autoDetectRenderer = PIXI.autoDetectRenderer;
+	renderer = autoDetectRenderer(canvas.width, canvas.height, rendererOptions);
+	renderer.backgroundColor = 0xFFFFFF;
+	stage = new PIXI.Container();
+	display();	
 }
 
 function display(){
-  renderer.render(stage);
+	renderer.render(stage);
+}
+
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
 }
